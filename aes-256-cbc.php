9302c58ff964941ddbf32be49ec89e6a73f04ae1
Mert ERGÜDEN <mfurkanergdn@hotmail.com>
2025-02-21 12:42:04 +0300

    Update aes-256-cbc.php

--- aes-256-cbc.php
+++ aes-256-cbc.php
@@ -1,36 +1,30 @@
+<?php
+
+define('SECRET_KEY', getenv('SECRET_KEY') ?: 'gizliAnahtar');
+define('IV_SIZE', openssl_cipher_iv_length('aes-256-cbc'));
+
 function generateToken($data) {
-    // Anahtar oluştur
-    $key = "gizliAnahtar"; // Güvenli bir anahtar kullanmalısınız
-
-    // Veriyi şifrele
-    $encryptedData = openssl_encrypt($data, 'aes-256-cbc', $key, 0, $key);
-
-    // Şifrelenmiş veriyi URL güvenli hale getir
-    $urlSafeToken = rtrim(strtr(base64_encode($encryptedData), '+/', '-_'), '=');
-
-    return $urlSafeToken;
+    $iv = openssl_random_pseudo_bytes(IV_SIZE);
+    $encryptedData = openssl_encrypt($data, 'aes-256-cbc', SECRET_KEY, OPENSSL_RAW_DATA, $iv);
+    $token = base64_encode($iv . $encryptedData);
+    return rtrim(strtr($token, '+/', '-_'), '=');
 }
 
 function decryptToken($token) {
-    // Anahtar oluştur
-    $key = "gizliAnahtar"; // Güvenli bir anahtar kullanmalısınız
-
-    // URL güvenli tokeni orijinal hale getir
     $paddedToken = str_pad(strtr($token, '-_', '+/'), strlen($token) % 4, '=', STR_PAD_RIGHT);
-
-    // Tokeni çöz
-    $decryptedData = openssl_decrypt(base64_decode($paddedToken), 'aes-256-cbc', $key, 0, $key);
-
+    $decodedToken = base64_decode($paddedToken);
+    $iv = substr($decodedToken, 0, IV_SIZE);
+    $encryptedData = substr($decodedToken, IV_SIZE);
+    $decryptedData = openssl_decrypt($encryptedData, 'aes-256-cbc', SECRET_KEY, OPENSSL_RAW_DATA, $iv);
     return $decryptedData;
 }
 
-// Token oluştur
 $originalData = "Bu veri şifrelenecek";
 $token = generateToken($originalData);
-
-// Tokeni çöz
 $decodedData = decryptToken($token);
 
 echo "Orijinal Veri: " . $originalData . "<br>";
 echo "Oluşturulan Token: " . $token . "<br>";
 echo "Çözülen Veri: " . $decodedData . "<br>";
+
+?>
